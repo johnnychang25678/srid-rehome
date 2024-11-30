54915004c742340766ff9536b503df1310649dd6
judyzhangyf <judyzhangyifan@gmail.com>
2024-11-29 16:15:14 -0800

    Change default font and favicon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
     darkMode: ["class"],
@@ -56,6 +57,9 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		  fontFamily: {
+			sans: ['Tinos', ...defaultTheme.fontFamily.sans],
+		  },
   		keyframes: {
   			'accordion-down': {
   				from: {
